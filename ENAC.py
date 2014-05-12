4f9a18e6f51cc01ea9dfcbeeb76645250ef939b8
Remo Hertig <remo.hertig@bluewin.ch>
2014-05-12 18:17:06 +0200

    added ENAC

--- ENAC.py
+++ ENAC.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Mon May 12 14:52:55 2014
+
+@author: nairboon
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  9 14:36:24 2014
+
+@author: nairboon
+"""
+
+# -*- coding: utf-8 -*-
 
 from pybrain.rl.environments.cartpole import CartPoleEnvironment, DiscreteBalanceTask, CartPoleRenderer
 from pybrain.rl.agents import LearningAgent
@@ -8,28 +22,37 @@ from matplotlib import pyplot as plt
 
 from scipy import mean
 
+from pybrain.rl.learners.valuebased import NFQ, ActionValueNetwork, Q
+from pybrain.rl.learners.directsearch import ENAC
+
+
 from pybrain.rl.environments import cartpole as cp
 
 from learner import BNL, ActionValueBayesianNetwork
 
 # switch this to True if you want to see the cart balancing the pole (slower)
+
 import numpy
 
 import multiprocessing
+
+
 
 
 #def run(task, parameters):
 def run(arg):
     task = arg[0]
     parameters = arg[1]
-    #print "run with", task,parameters
-    
+    #print "run with", parameters
     
     seed = parameters["seed"]
    
 
     process_id = hash(multiprocessing.current_process()._identity)
     numpy.random.seed(seed + process_id)
+
+
+    
     
     render = False    
     plot = True
@@ -44,25 +67,32 @@ def run(arg):
     
     task_class = getattr(cp, task)
     task = task_class(env, parameters["MaxRunsPerEpisode"])
-    testtask = task_class(env, parameters["MaxRunsPerEpisodeTest"])
+    testtask = task_class(env, parameters["MaxRunsPerEpisodeTest"],desiredValue=None)
 
     #print "dim: ", task.indim, task.outdim
-    
-    # to inputs state and 4 actions
-    module = ActionValueBayesianNetwork(task.outdim, task.indim)
-    
 
-    learner = BNL()
+    from pybrain.tools.shortcuts import buildNetwork
+    from pybrain.rl.agents import OptimizationAgent
+    from pybrain.optimization import PGPE
+
+    module = buildNetwork(task.outdim, task.indim, bias=False)
+    # create agent with controller and learner (and its options)
+
     # % of random actions
-    learner.explorer.epsilon = parameters["ExplorerEpsilon"]
+    #learner.explorer.epsilon = parameters["ExplorerEpsilon"]
     
     
-    agent = LearningAgent(module, learner)
+    agent = OptimizationAgent(module, PGPE(storeAllEvaluations = True, maxEvaluations=None,desiredEvaluation=1, verbose=False,maxLearningSteps=None))
+#
+#    print agent
+#    from pprint import pprint
+#    pprint (vars(agent.learner))
+    
     testagent = LearningAgent(module, None)
     experiment = EpisodicExperiment(task, agent)
     testexperiment = EpisodicExperiment(testtask, testagent)
 
-
+    
     def plotPerformance(values, fig):
         plt.figure(fig.number)
         plt.clf()
@@ -80,7 +110,7 @@ def run(arg):
     for episode in range(0,m):
     	# one learning step after one episode of world-interaction
         experiment.doEpisodes(parameters["EpisodesPerLearn"])
-        agent.learn(1)
+        #agent.learn(1)
     
         #renderer.drawPlot()
         
@@ -92,16 +122,40 @@ def run(arg):
             #print "Evaluating at episode: ", episode
             
             #experiment.agent = testagent
-            r = mean([sum(x) for x in testexperiment.doEpisodes(parameters["TestWith"])])
+            #r = mean([sum(x) for x in testexperiment.doEpisodes(parameters["TestWith"])])
+            #for i in range(0,parameters["TestWith"]):
+            y = experiment.doEpisodes(parameters["TestWith"])
+            print y   
+                
+                
+            l = parameters["TestWith"]
+            resList = (agent.learner._allEvaluations)[-l:-1]
             
-            xx = testexperiment.doEpisodes(parameters["TestWith"])
-            print xx
+            print agent.learner._allEvaluations
+            from scipy import array
+
+            rLen = len(resList)
+            avReward = array(resList).sum()/rLen
+            print resList
             exit(0)
+#            print("Parameters:", agent.learner._bestFound())
+#            print(
+#                " Evaluation:", episode,
+#                " BestReward:", agent.learner.bestEvaluation,
+#                " AverageReward:", avReward)
+#            if agent.learner.bestEvaluation == 0:
+#                
+#                print resList[-20:-1]
+#                print "done"
+#                break
+            performance.append(avReward)
+            
+
             env.delay = False
             testagent.reset()
             #experiment.agent = agent
         
-            performance.append(r)
+#            performance.append(r)
             if plot:
                 plotPerformance(performance, pf_fig)
         
